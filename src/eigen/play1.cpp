5ce51eec4a7af3e9cfe1a40c83cd35e011e016aa
Kaiyan XIAO <kaiyan_xiao@163.com>
2018-05-16 23:04:25 +0100

    Finished the Getting Start

--- src/eigen/play1.cpp
+++ src/eigen/play1.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
-#include <eigen3/Eigen/Dense>
+#include <Eigen/Dense>
 
-using Eigen::MatrixXd;
+using namespace Eigen;
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    /* getting start - ex 1 */
     MatrixXd m(2, 2);
     m(0,0) = 3;
     m(1,0) = 2.5;
     m(0,1) = -1;
     m(1,1) = m(1,0) + m(0,1);
-    std::cout << m << std::endl;
-    //int i;
-    //std::cin >> i;
+    cout << m << endl;
+    MatrixXd n(2,3);
+    n << 1, 2, 3, 4, 5, 6;
+    cout << n << endl;
+    /* getting start - ex 2 */
+    m = MatrixXd::Random(3, 3);
+    m = (m + MatrixXd::Constant(3, 3, 1.2)) * 50;
+    cout << "m = " << endl << m << endl;
+    VectorXd v(3);
+    v << 1, 2, 3;
+    cout << "m * v = " << endl << m * v << endl;
+    Matrix3d mm = Matrix3d::Random();
+    mm = (mm + Matrix3d::Constant(1.2)) * 50;
+    cout << "mm = " << endl << mm << endl;
+    Vector3d vv(1, 2, 3);
+    cout << "mm * vv = " << endl << mm * vv << endl;
 }
