0b23ad5e4ee3bafb07bbb6ac549cebc1dc4f572f
Kaiyan XIAO <kaiyan_xiao@163.com>
2018-05-16 01:15:16 +0100

    hello world of Eigen3 tutorial

---
+++ src/eigen/play1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <eigen3/Eigen/Dense>
+
+using Eigen::MatrixXd;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    MatrixXd m(2, 2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
+    //int i;
+    //std::cin >> i;
+}
