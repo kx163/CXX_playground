1fa4f33d04332881d1bfe8e1aeb78453809c459d
Kaiyan XIAO <kaiyan_xiao@163.com>
2018-05-28 15:42:27 +0100

    Slambook chapter 4 added SO3 and SE3 disturbation model calculation.

--- slambook/ch4/useSophus.cpp
+++ slambook/ch4/useSophus.cpp
@@ -37,6 +37,13 @@ int main(int argc, char const *argv[])
     Sophus::SO3 SO3_updated = Sophus::SO3::exp(update_so3) * SO3_R;
     cout << "Updated SO3 = " << SO3_updated << endl;
 
+    // verify left hand disturbation model
+    Eigen::Vector3d p (1, 1, 1);  // define the initial point
+    Eigen::Vector3d p_direct = Sophus::SO3::exp(update_so3) * (SO3_R * p);  // calculate final point by direct computation
+    cout << "p after rotation by direct computation: " << p_direct.transpose() << endl;
+    Eigen::Vector3d p_disturb = SO3_R * p + (- Sophus::SO3::hat(SO3_R * p)) * update_so3;  // calculate final point by disturbation model
+    cout << "p after rotation by disturbation model: " << p_disturb.transpose() << endl;
+
     cout << "*************************" << endl;
 
     /* SE3 section */
@@ -59,5 +66,16 @@ int main(int argc, char const *argv[])
     update_se3(0, 0) = 1e-4;
     Sophus::SE3 SE3_updated = Sophus::SE3::exp(update_se3) * SE3_Rt;
     cout << "Updated SE3 = " << SE3_updated << endl;
+
+    // verify left hand disturbation model
+    p_direct = Sophus::SE3::exp(update_se3) * (SE3_Rt * p);  // calculate final point by direct computation
+    cout << "p after transformation by direct computation: " << p_direct.transpose() << endl;
+    Eigen::Matrix<double, 4, 6> derivative;  // declare the derivative matrix
+    derivative.setZero();
+    derivative.block<3, 3>(0, 0) = Eigen::Matrix3d::Identity();
+    derivative.block<3, 3>(0, 3) = - Sophus::SO3::hat(SE3_Rt * p);
+    p_disturb = (SE3_Rt * p) + (derivative * update_se3).block<3, 1>(0, 0);  // calculate final point by disturbation model
+    cout << "p after transformation by disturbation model: " << p_disturb.transpose() << endl;
+
     return 0;
 }
